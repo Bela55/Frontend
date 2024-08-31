e8378282f4744b735055412306be80f431dcd3de
Isabela Oliveira <costbela02@gmail.com>
2024-08-30 21:53:17 -0300

    Introdução - Aula04

---
+++ aulas/aula04/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css'
+
+function Logo(props) {
+    const texto = "Logo da Aplicação";
+    
+    return (
+        <img calssName="Logo" scr={url} alt={texto}/>
+    );
+}
+
+export default Logo;
