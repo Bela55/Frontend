3c3b848614503d00af9e1a79ebc0bdac335d8db0
Isabela Oliveira <isabelaoliveirac@gmail.com>
2024-11-05 20:26:21 -0300

    Metade da aula11

---
+++ aulas/aula11/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
+function Login() {
+  const { login } = useContext(AuthContext);
+  return (
+    <>
+      <h1>Login</h1>
+      <button
+        onClick={(e) => login({ email: "isa@iesb.br", senha: "abcd1234" })}
+      >
+        Entrar
+      </button>
+    </>
+  );
+}
+
+export default Login;
