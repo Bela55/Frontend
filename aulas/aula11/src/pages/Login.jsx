3b4f5def4bfc2d7bc45fb68b3e9a0568cbbcad90
Isabela Oliveira <isabelaoliveirac@gmail.com>
2024-11-05 21:53:51 -0300

    Parte da aula 11

--- aulas/aula11/src/pages/Login.jsx
+++ aulas/aula11/src/pages/Login.jsx
@@ -1,16 +1,19 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 
 function Login() {
-  const { login } = useContext(AuthContext);
+  const { login, msg } = useContext(AuthContext);
   return (
     <>
       <h1>Login</h1>
+      {msg && <p>{msg}</p>}
       <button
         onClick={(e) => login({ email: "isa@iesb.br", senha: "abcd1234" })}
       >
         Entrar
       </button>
+      <Link to="/registrar">Registrar</Link>
     </>
   );
 }
